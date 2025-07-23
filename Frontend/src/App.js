510ae7008a151e4a42148d18cf26255c097ee4fc
yasar <yasarfarfat987@gmail.com>
2025-07-23 23:54:16 +0530

    routes added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Navbar from './components/Navbar';
+import Dashboard from './pages/Dashboard';
+import Upload from './pages/Upload';
+import TableView from './pages/TableView';
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
         <Navbar />
+        <main className="container mx-auto px-4 py-6">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/upload" element={<Upload />} />
+            <Route path="/table" element={<TableView />} />
+          </Routes>
+        </main>
         <Toaster position="top-right" />
       </div>
     </Router>
